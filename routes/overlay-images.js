8bb7cc4943f7decd5b0da32f78ab4b6d9e03b600
yobi7979 <yobi7979@gmail.com>
2025-10-11 10:05:36 +0900

    Fix TEAMLOGO static file serving - Add static file serving for TEAMLOGO folder in overlay-images router - Support Korean filenames with UTF-8 encoding - Set proper Content-Type for image files - Add cache control for team logo files - Resolve 404 errors for team logo images

--- routes/overlay-images.js
+++ routes/overlay-images.js
@@ -7,6 +7,42 @@ const path = require('path');
 const fs = require('fs').promises;
 const fsSync = require('fs');
 
+// TEAMLOGO 폴더에 대한 정적 파일 서빙
+router.use('/TEAMLOGO', express.static(path.join(__dirname, '../public/TEAMLOGO'), {
+  setHeaders: (res, filePath) => {
+    // 한글 파일명을 위한 인코딩 설정
+    const fileName = path.basename(filePath);
+    if (/[가-힣]/.test(fileName)) {
+      res.setHeader('Content-Disposition', 'inline; filename*=UTF-8\'\'' + encodeURIComponent(fileName));
+      
+      // 파일 확장자에 따른 Content-Type 설정
+      const ext = path.extname(fileName).toLowerCase();
+      let contentType = 'application/octet-stream';
+      
+      switch (ext) {
+        case '.png':
+          contentType = 'image/png';
+          break;
+        case '.jpg':
+        case '.jpeg':
+          contentType = 'image/jpeg';
+          break;
+        case '.gif':
+          contentType = 'image/gif';
+          break;
+        case '.webp':
+          contentType = 'image/webp';
+          break;
+      }
+      
+      res.setHeader('Content-Type', contentType + '; charset=utf-8');
+    }
+    
+    // 팀로고 파일에 대한 캐시 설정
+    res.setHeader('Cache-Control', 'public, max-age=3600');
+  }
+}));
+
 // 모델들
 const { SportOverlayImage, SportActiveOverlayImage, Match, Sport } = require('../models');
 
